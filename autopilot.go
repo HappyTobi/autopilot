939355760439b60450b4d1daea589d8747ca8892
Tobias Schug <Tobias.schug@porsche.de>
2019-01-17 10:49:39 +0100

    [UPDATE] version to 0.0.9

--- autopilot.go
+++ autopilot.go
@@ -162,7 +162,7 @@ func (AutopilotPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 0,
-			Build: 8,
+			Build: 9,
 		},
 		Commands: []plugin.Command{
 			{
